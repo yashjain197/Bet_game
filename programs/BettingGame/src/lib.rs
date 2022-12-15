bbd75ad88927feea9646f49112ecfbcd80ef5fcf
Yash Jain <yashjain19072001@gmail.com>
2022-12-16 01:08:17 +0530

    Commit

--- programs/BettingGame/src/lib.rs
+++ programs/BettingGame/src/lib.rs
@@ -20,6 +20,7 @@ pub mod betting_game {
         campaign.admin = *ctx.accounts.user.key;
         campaign.bettingAmount = bettingAmount;
         campaign.creationTime = clock.unix_timestamp;
+        campaign.is_campaign = true;
         Ok(())
     }
 
@@ -42,11 +43,17 @@ pub mod betting_game {
         Ok(())
     }
 
-    pub fn pay(ctx: Context<Pay>, amount: u64) -> ProgramResult{
+    pub fn pay(ctx: Context<Pay>) -> ProgramResult{
+        //Check for valid token to pay
+
         let campaign = &mut ctx.accounts.campaign;
+        if campaign.is_campaign{
         let clock = Clock::get()?;
         let currTime = clock.unix_timestamp;
-        if campaign.creationTime - currTime >= 1 {
+        let mut amount = campaign.bettingAmount;
+        if campaign.creationTime - currTime >= 86400 {
+            //call the endCampaign and give 10% commission to admin
+            return Err(ProgramError::InsufficientFunds);
             }
 
         let ix = anchor_lang::solana_program::system_instruction::transfer(
@@ -63,7 +70,9 @@ pub mod betting_game {
             ]
         );
         (&mut ctx.accounts.campaign).amount_given += amount;
-
+        let mut players = &mut ctx.accounts.campaign.players;
+        players.push(ctx.accounts.user.key());
+    }
         Ok(())
     }
 
@@ -72,21 +81,77 @@ pub mod betting_game {
         let user = &mut ctx.accounts.user;
 
         let commissionAmt = campaign.amount_given/10;
-        if campaign.admin == *user.key{
-           
+        
+        if campaign.amount_given>0{
         let ix = anchor_lang::solana_program::system_instruction::transfer(
-            &ctx.accounts.user.key(),
             &ctx.accounts.campaign.key(),
+            &ctx.accounts.campaign.admin.key(),
             commissionAmt
         );
-        }
+
+        anchor_lang::solana_program::program::invoke(
+            &ix,
+            &[
+                ctx.accounts.campaign.to_account_info(),
+                ctx.accounts.user.to_account_info(),
+            ]
+        );
+
+    }
+    
+
         Ok(())
+    }
+
+    pub fn winner(ctx: Context<Winner>) -> ProgramResult {
+        let campaign = &mut ctx.accounts.campaign;
+
+        let winnerOneAmt = campaign.amount_given/2;
+        let winnerThreeAmt = campaign.amount_given*3/10;
+        let winnerTwoAmt = campaign.amount_given;
+
+        let mut players = &campaign.players;
+
+        if campaign.amount_given>0{
+            let ix = anchor_lang::solana_program::system_instruction::transfer(
+                &campaign.key(),
+                &players[0].key(),
+                winnerOneAmt
+            );
+
+            // anchor_lang::solana_program::program::invoke(
+            //     &ix,
+            //     &[
+            //         campaign.to_account_info(),
+            //         connection.getAccountInfo(new PublicKey(players[0].key())),
+    
+            //     ]
+            // );
+    } 
+       
+    if campaign.amount_given>0{
+            let ix = anchor_lang::solana_program::system_instruction::transfer(
+                &campaign.key(),
+                &players[1].key(),
+                winnerTwoAmt
+            );
+    } 
+
+    if campaign.amount_given>0{
+            let ix = anchor_lang::solana_program::system_instruction::transfer(
+                &campaign.key(),
+                &players[2].key(),
+                winnerThreeAmt
+            );
+    } 
+
+    Ok(())
     }
 }
 
 #[derive(Accounts)]
 pub struct Create<'info>{
-    #[account{init,payer = user, space = 9000, seeds=[b"DEMO".as_ref(), user.key().as_ref()],bump}]
+    #[account{init,payer = user, space = 200, seeds=[b"DEMO".as_ref(), user.key().as_ref()],bump}]
     pub campaign: Account<'info, Campaign>,
 
     #[account{mut}]
@@ -123,6 +188,17 @@ pub struct Pay<'info>{
     pub system_program: Program<'info, System>
 }
 
+#[derive(Accounts)]
+pub struct Winner<'info>{
+    #[account(mut)]
+    pub campaign: Account<'info, Campaign>,
+
+    #[account(mut)]
+    pub user: Signer<'info>,
+
+    pub system_program: Program<'info, System>
+}
+
 
 #[account]
 pub struct Campaign{
@@ -131,7 +207,9 @@ pub struct Campaign{
     pub description: String,
     pub amount_given: u64,
     pub bettingAmount: u64,
-    pub creationTime: i64
+    pub creationTime: i64,
+    pub players: Vec<Pubkey>,
+    pub is_campaign: bool,
 }
 
 // #[error]
